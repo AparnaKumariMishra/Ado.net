f766857bd9ebca29b468dcfbc34165bf9d810b23
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-22 21:57:20 +0530

    Add files via upload
    UC4

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace EMPD4
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Refactor_EMPD4 obj = new Refactor_EMPD4();
+            Employee emp = new Employee();
+
+            emp.employeename = "Jack";
+            emp.basicpay = 16000;
+
+            obj.UpdateSalary(emp);
+        }
+    }
+}
