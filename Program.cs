054cb62ca530ab286887f3bf9e30179f8b6f4634
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-22 21:54:22 +0530

    Add files via upload
    UC2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace EMPD2
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            EMPD2 obj = new EMPD2();
+            obj.retrieve_data_from_database();
+        }
+    }
+}
