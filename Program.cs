c8a0a4d0e1446736664c209ec675aeeb7ab4354b
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-22 21:55:03 +0530

    Add files via upload
    UC3

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace EMPD3
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            EMPD3 obj = new EMPD3();
+            Employee emp = new Employee();
+
+            emp.employeeid = 5;
+            emp.employeename = "Terrisa";
+            emp.basicpay = 20000;
+
+            obj.UpdateSalary(emp);
+        }
+    }
+}
