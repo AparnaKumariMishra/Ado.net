2eb2de7022b32fd104445d50e34e09941546d3a1
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-22 21:53:29 +0530

    Add files via upload
    UC1

---
+++ EMPD1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace EmployeePayRollDataBaseSystem
+{
+    public class EMPD1
+    {
+        public static string str = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=payroll_service;Integrated Security=True";
+        public void connect_database()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(str);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Create Database Ado_Payroll_Service", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        //static void Main(string[] args)
+        //{
+          //  EMPD1 obj = new EMPD1();
+            //obj.connect_database();
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace EmployeePayRollDataBaseSystem
+{
+    public class program
+    {
+      public  static void Main(string[] args)
+        {
+            EMPD1 obj = new EMPD1();
+            obj.connect_database();
+        }
+    }
+}
